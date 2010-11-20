fd80652ddfea03fdcab1f7df4de1f6e41fe118ba
John Barnette <jbarnette@gmail.com>
2010-11-19 16:23:50 -0800

    Fixes for FS.

--- lib/rubygems/source/fs.rb
+++ lib/rubygems/source/fs.rb
@@ -1,3 +1,4 @@
+require "rubygems/installable"
 require "rubygems/package"
 
 module Gem
@@ -6,10 +7,6 @@ module Gem
       include Gem::Source
 
       attr_reader :display
-
-      # An Array of paths to .gem files in this source.
-
-      attr_reader :files
 
       # Path to the .gem file or directory for this source.
 
@@ -21,24 +18,43 @@ module Gem
 
       def initialize url
         @display  = url.to_s
-        @files    = []
         @path     = File.expand_path url.path
+      end
+
+      # An Array of paths to .gem files in this source.
+
+      def files
+        return @files if defined? @files
+
+        files = []
 
         if File.file? path
-          raise "#{path} isn't a .gem file." unless /\.gem/i =~ path
+          unless /\.gem/i =~ path
+            raise Gem::Exception, "#{path} isn't a .gem file."
+          end
+
           files << path
         else
-          files.concat Dir["#{path}/*.gem"]
+          files.concat Dir[File.join path, "*.gem"]
         end
+
+        @files = files
       end
 
       def pull name, version
-        spec = specs.search(name, version).first
-        raise "I don't have a spec for #{name}-#{version}." unless spec
-        Gem::Installable.new spec
+        spec = specs.search(name, Gem::Version.create(version)).first
+        raise Gem::Exception, "Can't find  #{name}-#{version}." unless spec
+        Gem::Installable::File.new File.join(path, spec.file_name)
+      end
+
+      def reset
+        @files = nil
+        @specs = nil
       end
 
       def specs
+        return @specs if defined? @specs
+
         specs = files.map do |file|
           File.open file, "rb" do |f|
             Gem::Package.open f, "r" do |package|
